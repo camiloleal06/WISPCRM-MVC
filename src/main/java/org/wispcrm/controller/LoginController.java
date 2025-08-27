89c94be5889bc263a739218c2e7fa7385bd7642b
Camilo Leal <cjleal@nequi.com>
2025-08-27 08:04:29 -0500

    feat : fixed logs on LoginController

--- src/main/java/org/wispcrm/controller/LoginController.java
+++ src/main/java/org/wispcrm/controller/LoginController.java
@@ -2,6 +2,7 @@ package org.wispcrm.controller;
 
 import java.security.Principal;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@Slf4j
 public class LoginController {
 
 	@GetMapping("/login")
@@ -18,11 +20,13 @@ public class LoginController {
 		
 		if(principal != null) {
 			flash.addFlashAttribute("info", "Ya ha inciado sesión anteriormente");
+			log.info("successLogin", "Inicio de Sesion correcto");
 			return "redirect:/";
 		}
 		
 		if(error != null) {
 			model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo!");
+			log.info("ErrorLogin","error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo!");
 		}
 		
 		if(logout != null) {
